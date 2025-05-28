fdf5c0ed7df82725ea6bdeec94e8140abbfbd210
All Hands On Tech <113864200+TeamAllHandsOnTech@users.noreply.github.com>
2025-05-27 18:47:24 -0700

    fixed kiwi code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/kiwiDriveBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/kiwiDriveBasic.java
@@ -1,53 +1,64 @@
 package org.firstinspires.ftc.teamcode.teleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Basic Kiwi Drive", group="B")
 public class kiwiDriveBasic extends LinearOpMode {
 
     // Declare OpMode members.
-    private DcMotor motor1;
-    private DcMotor motor2;
-    private DcMotor motor3;
+    private DcMotor motorFront;
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motorFront = hardwareMap.get(DcMotor.class, "front");
+        motorLeft = hardwareMap.get(DcMotor.class, "left");
+        motorRight = hardwareMap.get(DcMotor.class, "right");
 
-        motor1.setDirection(DcMotor.Direction.FORWARD);
-        motor2.setDirection(DcMotor.Direction.FORWARD);
-        motor3.setDirection(DcMotor.Direction.FORWARD);
+        motorFront.setDirection(DcMotor.Direction.REVERSE);
+        motorLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorRight.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            double driveX = gamepad1.left_stick_x;
-            double driveY = gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x * -0.3;
+            double driveX = gamepad1.left_stick_x * -1;
+            double driveY = gamepad1.left_stick_y *  1;
+            double turn   = gamepad1.right_stick_x * -0.6;
 
-            double power1 = driveX+turn;
-            double power2 = -0.5*driveX + 0.866*driveY + turn;
-            double power3 = -0.5*driveX - 0.866*driveY + turn;
-            double max = Math.max(Math.abs(power1), Math.max(Math.abs(power2), Math.abs(power3)));
+            double powerFront = driveX+turn;
+            double powerLeft = -0.5*driveX + 0.866*driveY + turn;
+            double powerRight = -0.5*driveX - 0.866*driveY + turn;
+            double max = Math.max(Math.abs(powerFront), Math.max(Math.abs(powerLeft), Math.abs(powerRight)));
 
             if (max<1) {max = 1;}
 
-            telemetry.addData("Power 1: ", power1);
-            telemetry.addData("Power 2: ", power2);
-            telemetry.addData("Power 3: ", power3);
+            telemetry.addData("Power Front: ", powerFront);
+            telemetry.addData("Power Left: ", powerLeft);
+            telemetry.addData("Power Right: ", powerRight);
 
-            motor1.setPower(power1/max);
-            motor2.setPower(power2/max);
-            motor3.setPower(power3/max);
+            motorFront.setPower(powerFront/max);
+            motorLeft.setPower(powerLeft/max);
+            motorRight.setPower(powerRight/max);
+
+            if (gamepad1.y) {
+                motorFront.setPower(0.75);
+            }
+            if (gamepad1.x) {
+                motorLeft.setPower(0.75);
+            }
+            if (gamepad1.b) {
+                motorRight.setPower(0.75);
+            }
+
+            telemetry.update();
         }
     }
 }
